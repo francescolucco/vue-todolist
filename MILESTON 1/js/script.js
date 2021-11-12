3e5a20ec4f9a3198048652ab919fe7fad6594f6f
francescolucco <francescolucco@googlemail.com>
2021-11-12 15:51:31 +0100

    first commit

---
+++ MILESTON 1/js/script.js
@@ -0,0 +1,40 @@
+const app = new Vue({
+
+    el: '#app',
+
+    data:[
+        {
+            text: 'Giocare con un cucciolo di elefante',
+            done: false
+        },
+        {
+            text: 'Camminare sulla Grande Muraglia Cinese',
+            done: false
+        },
+        {
+            text: 'Vedere il Carnevale di Venezia',
+            done: false
+        },
+        {
+            text: 'Imparare VueJS',
+            done: false
+        },
+        {
+            text: 'Fare il bagno nei mari della California',
+            done: false
+        },
+        {
+            text: 'Esplorare la Foresta Blu di Hallerbos, in Belgio',
+            done: false
+        },
+        {
+            text: 'Fare il giro del Lago di Garda in canoa',
+            done: false
+        },
+        {
+            text: 'Mangiare una fiorentina da 1kg in Piazza dei Signori a Firenze',
+            done: false
+        }
+    ],
+
+})
