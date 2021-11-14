6dc342e78bb3b190b415727ec269a4b322b57827
francescolucco <francescolucco@googlemail.com>
2021-11-14 20:26:27 +0100

    aggiunta bottone - milestone 3

---
+++ MILESTON 3/js/script.js
@@ -0,0 +1,54 @@
+const app = new Vue({
+
+    el: '#app',
+
+    data: {
+    
+       items: [
+           {
+               text: 'Camminare sulla Grande Muraglia Cinese',
+               done: false
+            },
+            {
+                text: 'Vedere il Carnevale di Venezia',
+                done: false
+            },
+            {
+                text: 'Imparare VueJS',
+                done: false
+            },
+            {
+                text: 'Fare il bagno nei mari della California',
+                done: false
+            },
+            {
+                text: 'Esplorare la Foresta Blu di Hallerbos, in Belgio',
+                done: false
+            },
+            {
+                text: 'Fare il giro del Lago di Garda in canoa',
+                done: false
+            },
+            {
+                text: 'Mangiare una fiorentina da 1kg in Piazza dei Signori a Firenze',
+                done: false
+            },
+            {
+                text: 'Giocare con un cucciolo di elefante',
+                done: false
+            }
+    ],
+    stringToDO: '',
+},
+
+    //Funzione per eliminare un oggetto dall'array
+    methods: {
+        textRemove(index){
+            this.items.splice(index, 1);
+        }
+    },
+
+    //creo una funzione per inserire i dati inseriti nell'area input nell'arrey di oggetti
+
+})
+
